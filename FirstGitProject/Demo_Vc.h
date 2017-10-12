48de8bbd1aef043e5cb47f353edd7c8167958f55
Prashant <prashant@prashant.local>
2017-10-12 13:25:58 +0530

    second Commit create Demo_Vc class

---
+++ FirstGitProject/Demo_Vc.h
@@ -0,0 +1,13 @@
+//
+//  Demo_Vc.h
+//  FirstGitProject
+//
+//  Created by Prashant on 12/10/17.
+//  Copyright © 2017 Prashant. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Demo_Vc : UIViewController
+
+@end
---
+++ FirstGitProject/Demo_Vc.m
@@ -0,0 +1,29 @@
+//
+//  Demo_Vc.m
+//  FirstGitProject
+//
+//  Created by Prashant on 12/10/17.
+//  Copyright © 2017 Prashant. All rights reserved.
+//
+
+#import "Demo_Vc.h"
+
+@interface Demo_Vc ()
+
+@end
+
+@implementation Demo_Vc
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSString *Str   =  @"Testing Git";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+  
+}
+
+
+
+@end
